cce7ca3d894a2687dea0ea57bf194703a6a75181
João Felipe Miranda Bonifácio <74162506+joaomboni@users.noreply.github.com>
2021-01-24 20:28:22 -0300

    Update controle_empresa.c

--- controle_empresa.c
+++ controle_empresa.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+/*a Uma empresa determinou um reajuste salarial de 5% a todos
+os seus funcionários. Além disto, concedeu um abono de R$ 100,00 para aqueles que recebem até R$ 750,00. Dado o valor do salário de um funcionário,
+informar para quanto ele será reajustado.*/
 
 void main()
 {
