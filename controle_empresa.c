966a799e878a36643a0b0264a58b90fdb5fab081
João Felipe Miranda Bonifácio <74162506+joaomboni@users.noreply.github.com>
2021-01-24 20:27:00 -0300

    controle de salario

---
+++ controle_empresa.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void main()
+{
+    float salario, salario1, salario2, salabono;
+    printf("Informe seu salario:\n");
+    scanf("%f", &salario);
+    salario1 = salario * 0.05;
+    salario2 = salario + salario1;
+    printf("\nSeu novo salario e: %.2f", salario2);
+    printf("\nAperte < ENTER > para seguir no app:");
+    getch();
+    if (salario2 <=750.00){
+        salabono = salario2 + 100.00;
+        printf("\nVoce recebeu um abono de 100.00, PARABENS: %.2f", salabono);
+    }
+    else {
+        printf("\nSeu novo salario e: %.2f", salario2);
+    }
+    getch();
+    return 0;
+}
