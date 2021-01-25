7ce13f6108680b6f1c48fe0edef362ffa8d694d0
João Felipe Miranda Bonifácio <74162506+joaomboni@users.noreply.github.com>
2021-01-25 13:25:54 -0300

    Update and rename main.c to v&f.c

--- v&f.c
+++ v&f.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/* Codifique uma instrução para exibir valores lógicos como
+true e false. Para o valor 0 deve aparecer false e para qualquer outro, true.*/
 int main()
 {
     int num;
